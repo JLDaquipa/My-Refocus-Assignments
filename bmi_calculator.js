09c8f88b298b2a616a7711b7b7e3e614f930c103
Jay Lloyd Daquipa <31461040+JLDaquipa@users.noreply.github.com>
2022-11-15 18:07:40 +0800

    Manual Testing for BMI Calculator.

--- bmi_calculator.js
+++ bmi_calculator.js
@@ -4,16 +4,16 @@ function calculateBMI(height, weight){
 
     let BMI = 0;
 
-    BMI = (weight / (height**2)).toFixed(2);
+    BMI = parseFloat((weight / (height**2)).toFixed(2));
 
     if(BMI < 18.5){
-        console.log(`Your BMI is ${BMI}, indicating your weight is in the Underweight category`);    
+          return `Your BMI is ${BMI}, indicating your weight is in the Underweight category`;    
     }else if(BMI > 18.5 && BMI < 24.9){
-         console.log(`Your BMI is ${BMI}, indicating your weight is in the Healthy Weight category`);
+          return `Your BMI is ${BMI}, indicating your weight is in the Healthy Weight category`;
     }else if(BMI > 25 && BMI < 29.9 ){
-         console.log(`Your BMI is ${BMI}, indicating your weight is in the Overweight category`);
+          return `Your BMI is ${BMI}, indicating your weight is in the Overweight category`;
     }else{
-         console.log(`Your BMI is ${BMI}, indicating your weight is in the Obese category`);
+          return `Your BMI is ${BMI}, indicating your weight is in the Obese category`;
     }
 
 }
@@ -25,4 +25,47 @@ function calculateBMI(height, weight){
 let height = 1.52; // in meters
 let weight = 60; // in kilograms
 
-calculateBMI(height, weight);
+console.log(calculateBMI(height, weight));
+
+
+// MANUAL UNIT TESTING
+
+function bmiCalculation(height, weight){
+     return parseFloat((weight / (height**2)).toFixed(2));
+}
+
+function testbmiCalculation(){
+     const result = bmiCalculation(1.52, 60);
+     const expected = 25.97;
+
+     console.assert(result === expected, `The result ${result} doesn't match the expected value ${expected}`);
+}
+
+testbmiCalculation(); // Passed
+
+
+
+function test1Calculator(){
+     const result = calculateBMI(1.52, 60);
+     const expected = "Your BMI is 25.97, indicating your weight is in the Overweight category";
+
+     console.assert(result === expected, `The result "${result}" doesn't match the expected value "${expected}"`);
+}
+
+function test2Calculator(){
+     const result = calculateBMI(1.85, 72);
+     const expected = "Your BMI is 21.04, indicating your weight is in the Healthy Weight category";
+
+     console.assert(result === expected, `The result "${result}" doesn't match the expected value "${expected}"`);
+}
+
+function test3Calculator(){
+     const result = calculateBMI(2.1336, 72.5);
+     const expected = "Your BMI is 15.93, indicating your weight is in the Underweight category";
+
+     console.assert(result === expected, `The result "${result}" doesn't match the expected value "${expected}"`);
+}
+
+test1Calculator(); // Passed
+test2Calculator(); // Passed
+test3Calculator(); // Passed
